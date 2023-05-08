930d4c9456f5994399989241a599f9e45e4a8626
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-08 23:32:20 +0300

    :sparkles: Added navigation feature

--- dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
@@ -12,18 +12,23 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
 import fi.tuni.dammyjson.ui.theme.DammyjsonTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,69 +44,13 @@ class MainActivity : ComponentActivity() {
                         .padding(3.dp),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        SearchField()
-                        CreateUserList()
-
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") { MainScreen(navController) }
+                        composable("AddUser") { AddUserScreen(navController) }
                     }
-
                 }
             }
         }
-    }
-}
-
-@Composable
-fun CreateUserList() {
-    val fetch = FetchTools()
-    var userData by remember { mutableStateOf(listOf<User>()) }
-    fetch.getData(url = "https://dummyjson.com/users", response = {
-        Log.d("test", it)
-        userData = fetch.parseDatatoObject(it) ?: emptyList()
-    }, failure = {
-        println(it)
-    })
-
-    UsersList(users = userData)
-}
-
-@Composable
-fun UsersList(users: List<User>) {
-    LazyColumn {
-        items(users) { user ->
-            Text("${user.firstName} ${user.lastName}", Modifier
-                .padding(10.dp),
-                fontSize = 25.sp
-            )
-        }
-    }
-}
-
-@Composable
-fun SearchField() {
-    var text by remember { mutableStateOf("") }
-    TextField(
-        value = text,
-        onValueChange = { newText -> text = newText},
-        placeholder = {Text("Hae")},
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(1.dp, Color.Gray)
-            .clip(
-                RoundedCornerShape(10.dp)
-            )
-    )
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DammyjsonTheme {
-        Greeting("Android")
     }
 }

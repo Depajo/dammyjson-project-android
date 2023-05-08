d98c05d2de46026fb0c8936345d2ff96a067b922
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-08 23:34:12 +0300

    :recycle: Move MainScreen functions MainScreen file

---
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/MainScreen.kt
@@ -0,0 +1,104 @@
+package fi.tuni.dammyjson
+
+import FetchTools
+import User
+import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(navControlle: NavController) {
+    Box {
+        Column {
+            SearchField()
+            CreateUserList()
+        }
+        AddButton(navControlle, modifier = Modifier
+            .padding(bottom = 20.dp, end = 20.dp)
+            .align(Alignment.BottomEnd))
+    }
+}
+
+@Composable
+fun AddUserScreen(navController: NavController) {
+    Text("Hello")
+    Button(
+        onClick = {
+            // Tee jotain uuden kohteen kanssa
+            navController.popBackStack()
+        }
+    ) {
+        Text("Add item")
+    }
+}
+
+@Composable
+fun CreateUserList() {
+    val fetch = FetchTools()
+    var userData by remember { mutableStateOf(listOf<User>()) }
+    fetch.getData(url = "https://dummyjson.com/users", response = {
+        Log.d("test", it)
+        userData = fetch.parseDatatoObject(it) ?: emptyList()
+    }, failure = {
+        println(it)
+    })
+
+    UsersList(users = userData)
+}
+
+@Composable
+fun UsersList(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->
+            Text("${user.firstName} ${user.lastName}", Modifier
+                .padding(10.dp),
+                fontSize = 25.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchField() {
+    var text by remember { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText},
+        placeholder = { Text("Hae") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Gray)
+            .clip(
+                RoundedCornerShape(10.dp)
+            )
+    )
+}
+
+@Composable
+fun AddButton(navControlle: NavController, modifier: Modifier = Modifier) {
+    FloatingActionButton(
+        onClick = { navControlle.navigate("AddUser") },
+        backgroundColor = Color.Cyan,
+        modifier = modifier
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "Add Button")
+    }
+}

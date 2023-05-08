aa16bba2f43e9a1b1385dae20b4a5245b4c358b8
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-08 21:39:22 +0300

    :sparkles: Add FetchTools file
    Add FetchTools file with three new functions getData, postData and parseDatatoObject.

---
+++ dammyjson/app/src/main/java/FetchTools.kt
@@ -0,0 +1,53 @@
+import android.util.Log
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+import com.fasterxml.jackson.module.kotlin.readValue
+
+
+class FetchTools {
+    fun getData(url: String, response: (String) -> Unit, failure: (IOException) -> Unit) {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                failure(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string()?.let { responseBody ->
+                    response(responseBody)
+                }
+            }
+        })
+    }
+
+    fun postData(url: String, json: String,
+                 response: (String) -> Unit, failure: (IOException) -> Unit) {
+        val client = OkHttpClient()
+        val reqBody = json.toRequestBody("application/json; charset=utf-8".toMediaType())
+        val request = Request.Builder().url(url).post(reqBody).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                failure(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string()?.let { responseBody ->
+                    response(responseBody)
+                }
+            }
+        })
+    }
+
+    fun parseDatatoObject(jsonData: String): List<User> {
+        Log.d("test", jsonData)
+        val objectMapper = ObjectMapper().registerModule(KotlinModule())
+        return objectMapper.readValue<Users>(jsonData).users
+    }
+}

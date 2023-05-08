62cf861a8091c981b59e66239348b555928a894b
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-08 17:36:29 +0300

    :construction: Add SearchField fuctionality

--- dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
@@ -3,31 +3,63 @@ package fi.tuni.dammyjson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fi.tuni.dammyjson.ui.theme.DammyjsonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             DammyjsonTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().padding(3.dp),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        SearchField()
+                    }
+
                 }
             }
         }
     }
 }
+
+@Composable
+fun SearchField() {
+    var text by remember { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText},
+        placeholder = {Text("Hae")},
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Gray)
+            .clip(
+                RoundedCornerShape(10.dp)
+            )
+    )
+}
+
+
+
 
 @Composable
 fun Greeting(name: String) {

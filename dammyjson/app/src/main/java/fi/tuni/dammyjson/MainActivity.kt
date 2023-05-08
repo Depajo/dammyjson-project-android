0f061d19b119d68630370bbe2554132a2ee3ab66
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-08 21:44:33 +0300

    :lipstick: Add new UI components UsersList and CreateUserList

--- dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/MainActivity.kt
@@ -1,13 +1,16 @@
 package fi.tuni.dammyjson
 
+import FetchTools
+import User
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,27 +20,59 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fi.tuni.dammyjson.ui.theme.DammyjsonTheme
 
 class MainActivity : ComponentActivity() {
+    val fetch = FetchTools()
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContent {
             DammyjsonTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize().padding(3.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(3.dp),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column() {
+                    Column {
                         SearchField()
+                        CreateUserList()
+
                     }
 
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CreateUserList() {
+    val fetch = FetchTools()
+    var userData by remember { mutableStateOf(listOf<User>()) }
+    fetch.getData(url = "https://dummyjson.com/users", response = {
+        Log.d("test", it)
+        userData = fetch.parseDatatoObject(it) ?: emptyList()
+    }, failure = {
+        println(it)
+    })
+
+    UsersList(users = userData)
+}
+
+@Composable
+fun UsersList(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->
+            Text("${user.firstName} ${user.lastName}", Modifier
+                .padding(10.dp),
+                fontSize = 25.sp
+            )
         }
     }
 }
@@ -57,9 +92,6 @@ fun SearchField() {
             )
     )
 }
-
-
-
 
 @Composable
 fun Greeting(name: String) {

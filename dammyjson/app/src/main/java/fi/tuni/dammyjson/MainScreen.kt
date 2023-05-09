f46fb138838222a5ec7656aa62e572ce981d7443
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-09 14:50:39 +0300

    :lipstick: Modify userList item to button and add action.

--- dammyjson/app/src/main/java/fi/tuni/dammyjson/MainScreen.kt
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/MainScreen.kt
@@ -3,15 +3,16 @@ package fi.tuni.dammyjson
 import FetchTools
 import User
 import android.util.Log
+import android.view.RoundedCorner
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
@@ -23,6 +24,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
 
 @Composable
 fun MainScreen(navControlle: NavController) {
@@ -55,10 +60,31 @@ fun CreateUserList() {
 fun UsersList(users: List<User>) {
     LazyColumn {
         items(users) { user ->
-            Text("${user.firstName} ${user.lastName}", Modifier
-                .padding(10.dp),
-                fontSize = 25.sp
-            )
+            Button(onClick = {
+                
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp)
+                .border(0.4.dp, Color.Gray),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            elevation = ButtonDefaults.elevation(defaultElevation = 0.dp)) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        "${user.firstName} ${user.lastName}",
+                        Modifier
+                            .padding(2.dp),
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Default,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                }
+            }
         }
     }
 }

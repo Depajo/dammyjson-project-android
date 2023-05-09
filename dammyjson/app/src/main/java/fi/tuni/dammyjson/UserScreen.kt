224b99b024e193ddb09021e38a91bb2b9ed3f580
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-09 22:36:49 +0300

    :lipstick: UserScreen show user data and added edit button.

--- dammyjson/app/src/main/java/fi/tuni/dammyjson/UserScreen.kt
+++ dammyjson/app/src/main/java/fi/tuni/dammyjson/UserScreen.kt
@@ -2,17 +2,23 @@ package fi.tuni.dammyjson
 
 import FetchTools
 import User
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -40,32 +46,63 @@ fun UserNotFound(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.align(Alignment.Center)) {
             CircularProgressIndicator()
-            Button(
-                onClick = {
-                    navController.popBackStack()
-                }
-            ) {
-                Text("Back")
-            }
         }
     }
-
 }
-
 @Composable
 fun UserFound(navController: NavController, user: User) {
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.align(Alignment.Center)) {
-            Text("Hello ${user.lastName}")
-            Button(
-                onClick = {
-                    navController.navigate("home")
-                }
-            ) {
-                Text("Back")
-            }
+        IconButton(Icons.Filled.ArrowBack, "Back", Modifier.align(Alignment.TopStart)) {
+            navController.popBackStack()
+        }
+        IconButton(Icons.Filled.Edit, "Back", Modifier.align(Alignment.TopEnd) ) {
+            navController.navigate("AddUser/${user.id}")
+        }
+        Column(modifier = Modifier
+            .align(Alignment.Center)
+            .padding(10.dp)) {
+            TextAreaWithLabel(label = "Name:", text = "${user.firstName} ${user.lastName}")
+            TextAreaWithLabel(label = "Phone:", text = user.phone)
+            TextAreaWithLabel(label = "Email:", text = user.email)
+            TextAreaWithLabel(label = "Age:", text = user.age.toString())
+            TextAreaWithLabel(label = "Username:", text = user.username)
+            TextAreaWithLabel(label = "Password:", text = user.password)
+
         }
 
     }
+}
+
+@Composable
+fun TextAreaWithLabel(label: String, text: String) {
+    Column(modifier = Modifier
+        .padding(10.dp)
+        .background(
+            if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray,
+            RoundedCornerShape(10.dp)
+        )
+        .fillMaxWidth()
+        .padding(10.dp))
+    {
+        Text(label, fontWeight = FontWeight.Bold, modifier = Modifier.padding(2.dp))
+        Text(text, modifier = Modifier.padding(2.dp))
+    }
+}
+
+@Composable
+fun IconButton(icon: ImageVector, description: String, modifier: Modifier, action: () -> Unit) {
+        FloatingActionButton(
+            modifier = modifier,
+            backgroundColor = if (isSystemInDarkTheme()) Color.Transparent else Color.White,
+            elevation = FloatingActionButtonDefaults.elevation(
+                defaultElevation = 0.dp,
+                pressedElevation = 0.dp
+            ),
+            onClick = {
+                action()
+            }
+        ) {
+            Icon(icon, contentDescription = description)
+        }
 
 }
